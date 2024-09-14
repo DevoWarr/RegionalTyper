7582e6ce62107e40aa5a418faee79ab41cfbdd15
DevoWarr <devowarr.dw@gmail.com>
2024-09-14 19:40:01 +0200

    Add project files.

---
+++ RegionalTyper/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace RegionalTyper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.lblRegionalText = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnToggle
+            // 
+            this.btnToggle.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToggle.Location = new System.Drawing.Point(18, 86);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(157, 44);
+            this.btnToggle.TabIndex = 0;
+            this.btnToggle.Text = "TOGGLE";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+            // 
+            // lblRegionalText
+            // 
+            this.lblRegionalText.AutoSize = true;
+            this.lblRegionalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegionalText.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblRegionalText.Location = new System.Drawing.Point(12, 21);
+            this.lblRegionalText.Name = "lblRegionalText";
+            this.lblRegionalText.Size = new System.Drawing.Size(332, 32);
+            this.lblRegionalText.TabIndex = 1;
+            this.lblRegionalText.Text = "Regional Typer Discord";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Red;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(222, 94);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(122, 29);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "INACTIVE";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 154);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblRegionalText);
+            this.Controls.Add(this.btnToggle);
+            this.Name = "MainForm";
+            this.Text = "Regional Discord Typer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToggle;
+        private System.Windows.Forms.Label lblRegionalText;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
---
+++ RegionalTyper/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace RegionalTyper
+{
+    public partial class MainForm : Form
+    {
+        private InputSimulator simulator = new InputSimulator();
+        private LowLevelKeyboardHook lkh = new LowLevelKeyboardHook();
+        private bool enableRegionalTyping = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            lkh.OnKeyPressed += lkh_OnKeyPressed;
+        }
+
+        void lkh_OnKeyPressed(object sender, Keys e)
+        {
+            if (enableRegionalTyping && Char.IsLetter((char)e))
+                ParseText(((char)e).ToString());
+        }
+
+        private void ParseText(string text)
+        {
+            string prefix = ":regional_indicator_";
+            string regional = $"{prefix}{text.ToLower()}: ";
+
+            lkh.UnHookKeyboard();
+            simulator.Keyboard.TextEntry(regional);
+            lkh.HookKeyboard();
+        }
+
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            enableRegionalTyping = !enableRegionalTyping;
+
+            if (enableRegionalTyping)
+            {
+                lkh.HookKeyboard();
+
+                lblStatus.Text = "ACTIVE";
+                lblStatus.BackColor = Color.Green;
+            }
+            else
+            {
+                lkh.UnHookKeyboard();
+
+                lblStatus.Text = "INACTIVE";
+                lblStatus.BackColor = Color.Red;
+            }
+        }
+    }
+}
